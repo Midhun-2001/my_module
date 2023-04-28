6f2aec7bd3d0405e6a70244f3a35addf0ef13486
Midhun Das L <95303776+Midhun-2001@users.noreply.github.com>
2023-04-28 11:09:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[],
-    author='Your Name',
-    author_email='your_email@example.com',
+    author='Midhun Das L',
+    author_email='midhundas996@gmail.com',
     description='A simple module that says hello!',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    url='https://github.com/your_username/my_module',
+    url='https://github.com/Midhun-2001/my_module',
 )
 
