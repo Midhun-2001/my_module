9f28f4c475f94df8429b352f67c5d52977d62178
Midhun Das L <95303776+Midhun-2001@users.noreply.github.com>
2023-04-28 11:07:17 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_module',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Your Name',
+    author_email='your_email@example.com',
+    description='A simple module that says hello!',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    url='https://github.com/your_username/my_module',
+)
+
